5bf11c76bba46467d51651479c7c07b6e003ffab
Mbareck21 <missara.lamine@gmail.com>
2022-10-16 12:02:10 -0500

    lesson 1.2

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import AddTodoForm from "./AddTodoForm";
 // import the TodoList
 import TodoList from "./TodoList";
 
-
 export default function App() {
   return (
     <div className="App">
-      
       <h1>Todo List</h1>
       {/* showing todo list */}
       <TodoList />
       {/* shoeing the input form */}
-      <AddTodoForm/>
+      <AddTodoForm />
     </div>
   );
 }
-
-
