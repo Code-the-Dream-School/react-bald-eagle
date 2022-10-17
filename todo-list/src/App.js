2c9c2b3ffc2a5cf9928a10d9fa257369d6fdaa7e
Alton Michaux <alton.michaux@yahoo.com>
2022-10-17 08:21:51 -0400

    setting up todo list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
 function App() {
+  const todoList = [
+    {
+      description: "Paint the kitchen",
+      id: 1
+    },
+    {
+      description: "Feed the cats",
+      id: 2
+    },
+    {
+      description: "Take the kids to the park",
+      id: 3
+    }
+  ]
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Todo List
+      </h1>
+
+      <label htmlFor="search">search</label>
+      <input id="search" type="text"></input>
+
+      <hr/>
+
+      <ul list-style-type="none">
+        { todoList.map(function (task) {
+          return <li key={task.id}><b>{task.description}</b></li>
+          })
+        }
+      </ul>
     </div>
   );
 }
