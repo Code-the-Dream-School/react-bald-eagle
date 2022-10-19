a252293de55c65278c29b05c59443faee64bed43
SimonjShurety <simonjshurety@gmail.com>
2022-10-19 20:26:28 -0300

    Add bucket list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
+
+const bucketList = [
+  { id: 1, title: "Walk over hot coals" },
+  { id: 2, title: "Learn to fly a helicopter" },
+  { id: 3, title: "Snorkel the great barrier reef" },
+  { id: 4, title: "Eat crocodile in the amazon" },
+  { id: 5, title: "Box a kangaroo" },
+];
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bucket List</h1>
+      <ul>
+        {bucketList.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
-
 export default App;
