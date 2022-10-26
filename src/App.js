33bf38ad78622ef819d7660ef6226092f0d256fd
Brittany Halterman <halterma@gmail.com>
2022-10-25 21:21:41 -0400

    Create AddTodoForm component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
 import TodoList from './TodoList';
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
+        <AddTodoForm />
         <TodoList />
     </>
   );
-}
+};
 
 export default App;
