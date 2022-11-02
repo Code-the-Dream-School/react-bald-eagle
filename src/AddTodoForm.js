f8b53913c89fb51c10ab807eacdc85c968f33975
Pramote S <pramote.suewiroj@gmail.com>
2022-11-02 14:15:55 -0700

    Submit Exercise 1.3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
-const AddTodoForm = () => {
+const AddTodoForm = (props) => {
+  let handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+    props.onAddTodo(todoTitle);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">
         Title
       </label>
-      <input id="todoTitle">
-      </input>
+      <input id="todoTitle" type="text" name="title" />
       <button>
         Add
-      </button>
+      </button> 
     </form>
   )
 }
--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = React.useState('');
+
   return (
     <div>
       <h1>Todo List</h1>
-      <div>
-        <AddTodoForm />
-        <TodoList />
-      </div>
+      
+      <AddTodoForm onAddTodo={setNewTodo} />
+
+      <hr/>
+
+      <p>
+        {newTodo}
+      </p>
+        
+      <TodoList />
+      
     </div>
   );
 }
