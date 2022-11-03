0b20ca66b239b6cd9ef8f98b8291b5ba39a993c6
Mbareck21 <missara.lamine@gmail.com>
2022-11-02 19:51:45 -0500

    adding more to lesson-1.3

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -2,5 +2,5 @@ import React from "react";
 // Declare a function named TodoListItem
 const TodoListItem = (props) => <li>{props.todo.title}</li>;
 
-// Export TodoListItem function as default module
+// Export TodoListItem function as default modu
 export default TodoListItem;
