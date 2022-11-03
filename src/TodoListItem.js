739470ffd78de1479c210f4d5f6eaa95a80b414a
Mbareck21 <missara.lamine@gmail.com>
2022-11-02 19:56:05 -0500

    new updates

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -2,5 +2,5 @@ import React from "react";
 // Declare a function named TodoListItem
 const TodoListItem = (props) => <li>{props.todo.title}</li>;
 
-// Export TodoListItem function as default modu
+// Export TodoListItem function as default 
 export default TodoListItem;
