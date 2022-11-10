b8856338d987eedafaed3a2b516bed69372933f6
Cecilia Rodriguez <100891844+cecerodrgz@users.noreply.github.com>
2022-11-10 18:50:32 -0500

    Revert "added todoform"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import * as React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
-import {useState} from 'react'; 
 
- function App(){
-  const [newTodo, setNewTodo] = useState("");
-   return (
-     <div>
-       <h1>Todo List</h1>
-       <AddTodoForm onAddTodo={setNewTodo}/>
-       <TodoList />
-       <p>{newTodo}</p>
-     </div>
+const todoList = [
+  {
+    id: '1',
+    title: 'Complete assignment'
+  },
+  {
+    id: '2',
+    title: 'Finish reading Road to React'
+  },
+  {
+    id: '3',
+    title: 'Listen to Podcast episode.'
+  }
 
-   )
- }
+]
 
- export default App;
+function App(){
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+      {todoList.map(function(item){
+          return <li key={item.id.title}>{item.title}</li>;
+      })}
+      </ul>
+    </div>
+ 
+  )
+}
+
--- src/TodoListItem.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const TodoListItem = (props) => <li>{props.todo.title}</li>;
-
-export default TodoListItem;
