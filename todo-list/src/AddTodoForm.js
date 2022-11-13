f69638fe02e5680185b84b991ba2663067fd6487
alton-michaux <alton.michaux@yahoo.com>
2022-11-13 12:07:20 -0500

    added state variables for todoTitle and setTodoTitle in AddTodoForm.js

--- todo-list/src/AddTodoForm.js
+++ todo-list/src/AddTodoForm.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const AddTodoForm = (props) => {
+    const [todoTitle, setTodoTitle] = React.useState('');
+
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value
+        setTodoTitle(newTodoTitle)
+    }
+
     const handleAddTodo = (event) => {
         event.preventDefault()
-        const todoTitle = event.target.title.value
-        console.log(todoTitle)
         props.onAddTodo(todoTitle)
     }
 
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="TodoTitle"> Title </label>
-            <input type="text" id="TodoTitle" name="title"/>
+            <input 
+                type="text" 
+                id="TodoTitle" 
+                name="title"
+                value={todoTitle}
+                onChange={handleTitleChange}
+            />
             <button type="submit"> Add </button>
         </form>
     )
