ae81725799548bdc59e9f07d08f4fbbeef9ced9d
Brittany Halterman <halterma@gmail.com>
2022-11-15 20:54:50 -0500

    Add new todo to list

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -2,25 +2,21 @@ import React, { useState } from "react";
 
 function AddTodoForm(props) {
 
-    // Create new state variable named todoTitle with setter setTodoTitle
     const [todoTitle, setTodoTitle] = useState('')
 
-    // declare a new function named handleTitleChange that takes event as a parameter
     function handleTitleChange(event) {
-        // retrieve the input value from the event object and store in variable named newTodoTitle
         const newTodoTitle = event.target.value;
-        // call the state setter setTodoTitle and pass newTodoTitle
         setTodoTitle(newTodoTitle);
     }
 
     function handleAddTodo(event) {
         event.preventDefault();
-        // remove the todoTitle variable
-            //var todoTitle = event.target.title.value;
-        // update onAddTodo callback handler to pass our todoTitle state variable
-        props.onAddTodo(todoTitle);
+        // Update the onAddTodo callback prop to pass an Object instead of a String
+        props.onAddTodo({title: todoTitle, id: Date.now()});
+            // Disclaimer: we are suggesting Date.now() for now as a placeholder for unique number generation, but in the future you should not use this
         console.log(todoTitle);
-        event.target.reset();
+        //remove the reset() method and replace it with logic to reset the todoTitle state to an empty String
+        setTodoTitle("");
     };
 
     return (
@@ -28,8 +24,6 @@ function AddTodoForm(props) {
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
 
-            {/* Add value prop equal to todoTitle from component props */} 
-            {/* Add onChange prop equal to handleTitleChange function reference */}
             <input 
                 type="text" 
                 id="todoTitle" 
--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import AddTodoForm from "./AddTodoForm";
 
 function App() {
   
-  const [newTodo, setNewTodo] = useState('');
+  //Remove the newTodo state variable and the corresponding JSX that displays it
+    // const [newTodo, setNewTodo] = useState('');
+
+  //Declare a new function named addTodo that takes newTodo as a parameter
+  function addTodo(newTodo) {
+    //Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
+    setTodoList([...todoList, newTodo]);
+  }
 
   const [todoList, setTodoList] = useState([]);
 
   return (
     <>
       <h1>Todo List</h1>
-        <AddTodoForm onAddTodo={setNewTodo} />
-        <p>{newTodo}</p>
-
+        {/* Change the value of the onAddTodo prop for AddTodoForm to addTodo */}
+        <AddTodoForm onAddTodo={addTodo} />
+        {/* <p>{newTodo}</p> */}
         <TodoList todoList={todoList}/>
     </>
   );
