c22df315ebc5536049982870f7f9ce019f738bf2
alton-michaux <alton.michaux@yahoo.com>
2022-11-16 20:11:03 -0500

    finally got the app working, had to encapsulate props in an array

--- todo-list/src/AddTodoForm.js
+++ todo-list/src/AddTodoForm.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const AddTodoForm = (props) => {
-    const [todoTitle, setTodoTitle] = React.useState('');
+  const [todoTitle, setTodoTitle] = React.useState("");
 
-    const handleTitleChange = (event) => {
-        const newTodoTitle = event.target.value
-        setTodoTitle(newTodoTitle)
-    }
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
 
-    const handleAddTodo = (event) => {
-        event.target.value = ''
-        props.onAddTodo({title: todoTitle, id: Date.now()})
-    }
+  const handleAddTodo = (event) => {
+    event.preventDefault()
+    props.onAddTodo({ title: todoTitle, id: Date.now() });
+  };
 
-    return (
-        <form onSubmit={handleAddTodo}>
-            <label htmlFor="TodoTitle"> Title </label>
-            <input 
-                type="text" 
-                id="TodoTitle" 
-                name="title"
-                value={todoTitle}
-                onChange={handleTitleChange}
-            />
-            <button type="submit"> Add </button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="TodoTitle"> Title </label>
+      <input
+        type="text"
+        id="TodoTitle"
+        name="title"
+        value={todoTitle}
+        onChange={handleTitleChange}
+      />
+      <button type="submit"> Add </button>
+    </form>
+  );
+};
 
 export default AddTodoForm;
--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -1,32 +1,12 @@
-import React from 'react';
-import TodoListItem from './TodoListItem';
-
-
-// const todoList = [
-// 	{
-// 		title: "Clean the kitchen",
-// 		id: 1
-// 	},
-// 	{
-// 		title: "Feed the kids",
-// 		id: 2
-// 	},
-// 	{
-// 		title: "Take the dogs to the park",
-// 		id: 3
-// 	}
-// ]
+import React from "react";
+import TodoListItem from "./TodoListItem";
 
 const TodoList = (props) => (
-	<ul style={{listStyleType: "none"}}>
-		{ console.log(props) }
-		{ props.todoList.map(function (todo) {
-				return (
-					< TodoListItem key={todo.id} todo={todo} />
-				)
-			})
-		}
-	</ul>
-)
+  <ul style={{ listStyleType: "none" }}>
+    {props.todoList.map(function (todo) {
+      return <TodoListItem key={todo.id} todo={todo} />;
+    })}
+  </ul>
+);
 
 export default TodoList;
