07af6a5ffadb066d5aebd5faaaeaeea97e4f3201
Brittany Halterman <halterma@gmail.com>
2022-11-29 20:49:00 -0500

    Save Todo List in Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  
+
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
 
-  const [todoList, setTodoList] = useState([]);
+  //Update the default state for todoList to read your "savedTodoList" item from localStorage
+  const [todoList, setTodoList] = useState(
+    // Update your default state to parse the value of the "savedTodoList" item
+    JSON.parse(localStorage.getItem("savedTodoList")) ?? []);
+
+  //Define a useEffect React hook with todoList as a dependency
+  useEffect(() => {
+    // convert todoList to a string
+    const todoString = JSON.stringify(todoList)
+    //Save the todoList inside localStorage with the key "savedTodoList"
+    localStorage.setItem("savedTodoList", todoString)
+  }, [todoList]);
 
   return (
     <>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,7 +4,7 @@ import TodoListItem from "./TodoListItem";
 function TodoList({todoList}) {
     return (
       <ul>
-        {todoList.map(function (todo) {
+        {todoList.map(function(todo) {
           return <TodoListItem key={todo.id} todo={todo}/>;
         })}
       </ul>
