8eb2e5fb992e04667ccedc232ddee6812fa91531
Mbareck21 <missara.lamine@gmail.com>
2022-12-11 12:03:23 -0600

    all is working

--- src/App.js
+++ src/App.js
@@ -4,68 +4,32 @@ import AddTodoForm from "./AddTodoForm";
 // import the TodoList
 import TodoList from "./TodoList";
 
-// export default function App() {
-//   const [todoList, setTodoList] = React.useState([]);
-//   const [isLoading, setIsLoading] = React.useState(true);
-
-//   React.useEffect(() => {
-//     new Promise((resolve, reject) =>
-//       setTimeout(() => resolve({ data: { todoList: todoList } }), 2000)
-//     ).then((result) => {
-//       setTodoList(result.data.todoList);
-//       setIsLoading(false);
-//     });
-//   });
-
-//   React.useEffect(() => {
-//     if (isLoading === false) {
-//       return todoList;
-//     }
-//   });
-
-//   //addTodo function
-//   function addTodo(newTodo) {
-//     setTodoList([...todoList, newTodo]);
-//   }
-//   //remove button
-//   const removeTodo = (id) => {
-//     const newTodolist = todoList.filter((todo) => id !== todo.id);
-//     setTodoList(newTodolist);
-//   };
-//   return (
-//     <>
-//       <h1>Todo List</h1>
-
-//       <AddTodoForm onAddTodo={addTodo} />
-//       {isLoading ? (
-//       <p>Loading...</p>
-
-//       ) : (
-
-//       <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-//       )}
-//     </>
-//   );
-// }
 export default function App() {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    new Promise((resolve, reject) =>
-      setTimeout(() => resolve({ data: { todoList: todoList } }), 2000)
-    ).then((result) => {
-      setTodoList(result.data.todoList);
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("todoList")) || [],
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList([...result.data.todoList]);
       setIsLoading(false);
     });
-  });
+  }, []);
 
   React.useEffect(() => {
+    // check that isLoading is false before setting localStorage
     if (isLoading === false) {
       localStorage.setItem("todoList", JSON.stringify(todoList));
+      return;
     }
   }, [todoList, isLoading]);
-
   // addTodo function
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
@@ -81,7 +45,6 @@ export default function App() {
     <>
       <h1>Todo List</h1>
 
-     
       <AddTodoForm onAddTodo={addTodo} />
 
       {isLoading ? (
