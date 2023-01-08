7ed9f623266c3a3cbca0a88708d3bd56a09d2c26
alton-michaux <alton.michaux@yahoo.com>
2023-01-08 11:16:50 -0500

    removing some unwanted code

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -9,30 +9,6 @@ const App = () => {
 
   const [isError, setIsError] = useState(false)
 
-  // this simulates asynchronous load when app is initialized
-  // useEffect(() => {
-  //   new Promise((resolve) => {
-  //     setTimeout(() => resolve({ data: { todoList: todoList } }),
-  //       2000
-  //     );
-  //   }).then((result) => {
-  //     setTodoList(result.data.todoList)
-  //     setIsLoading(false)
-  //   }).catch(() => {
-  //     setIsError(true)
-  //   })
-  // }, []);
-
-  // the above code was used beceause the following warning was being received
-  // in the console when using the useEffect hook
-
-  // Warning: Maximum update depth exceeded. 
-  // This can happen when a component calls setState inside useEffect, 
-  // but useEffect either doesn't have a dependency array, 
-  // or one of the dependencies changes on every render.
-
-  // the following block has been used instead:
-
   const asyncData = (items) => {
     new Promise((resolve) => {
       setTimeout(() => resolve({ data: { todoList: items } }),
