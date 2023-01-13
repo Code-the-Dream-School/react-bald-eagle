ca68c238b085d2857d06793755794dee0bd27919
Pramote S <pramote.suewiroj@gmail.com>
2023-01-12 21:42:14 -0800

    Exercise 1.8

--- src/App.js
+++ src/App.js
@@ -3,48 +3,47 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 import {useState, useEffect} from 'react';
 
-// const useSemiPersistentState = () => {
-
-//  const [todoList, setTodoList] = useState(
-//    JSON.parse(localStorage.getItem("savedTodoList")) || []
-//  );
-
-//  useEffect(() => {
-//     localStorage.setItem("savedTodoList", JSON.stringify(todoList))
-//   }, [todoList]);
-
-//   return [todoList, setTodoList];
-// };
-
 function App() {
 
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  const [todoList, setTodoList] = useState([]
+    // JSON.parse(localStorage.getItem("savedTodoList")) || []
   );
-
-  // excercise 1.7 Async
-
-useEffect(() => {
-  new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        data: {
-          todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
-        },
-      });
-    }, 2000);
-  }).then((result) => {
-    setTodoList([...result.data.todoList]);
-    setIsLoading(false);
-  });
-}, []);
-
   const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+    //   new Promise ((resolve, reject) => {
+    //     setTimeout(() => {
+    //       resolve({
+    //         data: {
+    //           todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
+    //         },
+    //       });
+    //     }, 2000);
+    //   }).then((result) => {
+    //     setTodoList([...result.data.todoList]);
+    //     setIsLoading(false);
+    //   }); 
+
+  useEffect(() => {
+  
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    }).then((response) => response.json())
+    .then((result) => {
+      setTodoList(result.records);
+      setIsLoading(false)
+    })
+  }, []);
+
+  
   useEffect(() => {
     if(!isLoading) {
       localStorage.setItem('savedTodoList', JSON.stringify(todoList));
     }
-  },[todoList,isLoading])
+  }, [todoList,isLoading])
 
   const [newTodo, setNewTodo] = useState(''); 
 
@@ -60,12 +59,10 @@ useEffect(() => {
   return (
     <>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? ( 
-        <p>Loading...</p> 
-      ) : (
+      <AddTodoForm onAddTodo={addTodo}/>
+      {isLoading ? (<p>Loading...</p>) : 
         <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-      )}
+      }
       
     </>
   );
