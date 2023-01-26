8b1fd44fa4bf559ab9b0493055778f747ecafa2d
Brittany Halterman <halterma@gmail.com>
2023-01-25 20:37:37 -0500

    Setup Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,26 @@ import React, { useEffect, useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from "./AddTodoForm";
 
+// Import BrowserRouter, Routes, and Route from react-router-dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Remove the placeholder "Promise"
-      // new Promise ((resolve, reject) => {
-      //   // ***Mimic a loading delay***
-      //   setTimeout(() => {
-      //     resolve({ data: {todoList: JSON.parse(localStorage.getItem("savedTodoList")) || []}, });
-      //   }, 2000)
 
-    // Using the Fetch API, create a "GET" request
-    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, { //url: the url of your request
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, {
       method: 'GET',
-      // add the property 'headers' as an object
       headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}` //the token to authorize the request
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
       },
     })
-    // Chain a then method to your fetch call and pass it a function that returns the response JSON data
     .then((response) => {
       return response.json();
     })
     .then((result) => {
-    // Update the setToDoList call to reference the new result format
     setTodoList(result.records);
     setIsLoading(false);
   });
@@ -51,21 +44,31 @@ function App() {
     )
     setTodoList([...modifiedTodo])
   };
-
-  return (
-    <React.Fragment>
-      <h1>Todo List</h1>
-        <AddTodoForm onAddTodo={addTodo} />
-        <div>
-          {isLoading ? 
-            <p>Loading...</p>
-           : 
-            <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-          }
-        </div>
-    </React.Fragment>
-  );
-
+return (
+  //  Wrap existing JSX within new BrowserRouter component
+  <BrowserRouter>
+    {/* Wrap existing JSX within new Routes component */}
+    <Routes>
+      {/* Wrap existing JSX within new Route component with prop path equal to the root path and prop exact */}
+      <Route exact path="/" element=
+      {
+        
+          <React.Fragment>
+            <h1>Todo List</h1>
+              <AddTodoForm onAddTodo={addTodo} />
+              <div>
+                {isLoading ? 
+                  <p>Loading...</p>
+                : 
+                  <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+                }
+              </div>
+          </React.Fragment>
+        
+      } />
+    </Routes>
+  </BrowserRouter>
+);
 };
 
 export default App;
