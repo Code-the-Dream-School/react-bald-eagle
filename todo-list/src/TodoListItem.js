0b5f42b234722110df5b69f09ae21c655e119316
alton-michaux <alton.michaux@yahoo.com>
2023-02-02 19:28:58 -0500

    implementing css modules

--- todo-list/src/TodoListItem.js
+++ todo-list/src/TodoListItem.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-sequences */
 import React from "react";
+import styles from "./TodoListItem.module.css"
 
 const TodoListItem = ({ todo, onRemoveTodo }) => {
   const removeTodo = () => {
     onRemoveTodo(todo.id)
   }
   return (
-    <li className="todo-item">
+    <li className={styles.itemTextColor}>
       <b>{todo.fields.Name} </b>
-      <button className="remove-todo btn btn-dark" type="button" onClick={removeTodo}>
+      <button className={`btn ${styles.removeTodo} btn-dark`} type="button" onClick={removeTodo}>
         Remove
       </button>
     </li>
