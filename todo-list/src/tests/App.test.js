22a76eaf70733b7dbd94473f5981f44d557e3ae5
alton-michaux <alton.michaux@yahoo.com>
2023-02-13 01:58:52 -0500

    successfully mocked first meaningful test!

--- todo-list/src/tests/App.test.js
+++ todo-list/src/tests/App.test.js
@@ -4,8 +4,12 @@ jest.mock('../App')
 
 const App = require("../App.js")
 
-test("return todoList object", () => {
+test("returns todoList object", () => {
   return App.fetchTodos().then(data => expect(data.records).not.toBeUndefined())
+})
+
+test("returns todoList object with properties", () => {
+  return App.fetchTodos().then(data => expect(data.records[0]["fields"].Name).toBe("remove the words from  your socks"))
 })
 
 
