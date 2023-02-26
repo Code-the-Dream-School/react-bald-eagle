e0050bec6883d3913bc344ef44e2400ec286da63
khanh.taduy <khanh.taduy@grabtaxi.com>
2023-02-26 18:48:43 +0700

    Cleaning stuff

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,65 +1,89 @@
-import React from 'react';
-import InputWithLabel from './InputWithLabel';
+import React from "react";
+import InputWithLabel from "./InputWithLabel";
 
+function AddToDoForm({ onAddTodo }) {
+  const [todoTitle, setTodoTitle] = React.useState("");
+  const [dueDate, setDueDate] = React.useState("");
+  // const [anotherTitle, setAnotherTitle] = React.useState("");
 
-export const addTasks = async ({todoTitle,formData }) => {
-  const res = await fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      'Content-Type': 'application/json', 
-    },
-    body: JSON.stringify({
-      records: [
-        {
-          fields: {
-            ...formData,
-          },  
-        },
-      ],
-    }),         
-  });
-const result2 = await res.json();    
-console.log("result2", result2);
-}; 
-
-function AddToDoForm ({ onAddTodo }) {
-  
-  const [todoTitle, setTodoTitle] = React.useState('');
-  
-  const handleTitleChange = (event) => {
-    console.log("event", event)
-    const newTodoTitle = event.target.value;
-    setTodoTitle(newTodoTitle);
-  }
-  const handleAddTodo = (event) => {
+  const handleAddTodo = async (event) => {
     event.preventDefault();
-    // const todoTitle = event.target.title.value
-    // const taskDueDate = event.target["Due Date"].value;
-    const todoTitle = event.target.title.value;
-    console.log(`todoTitle:`, todoTitle)
-    addTasks({todoTitle});
-    
-    // console.log(todoTitle);
-    // event.target.reset();
-    // onAddTodo({title: todoTitle, id: Date.now()});
-    // onAddTodo({ title: todoTitle });
-    // setTodoTitle('');
-  }
-  
+
+    // Preparing the payload to send to AirTable
+    const payload = {
+      title: todoTitle,
+      // dueDate: dueDate,
+    };
+
+    const res = await fetch(
+      `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          records: [
+            {
+              fields: {
+                ...payload,
+              },
+            },
+          ],
+        }),
+      }
+    );
+    const result2 = await res.json();
+    console.log("result2", result2);
+  };
+
+  const onChangeInput = (event) => {
+    const value = event.target.value;
+    const inputID = event.target.id;
+
+    if (inputID == "title") {
+      setTodoTitle(value);
+    }
+
+    if (inputID == "dueDate") {
+      setDueDate(value);
+    }
+
+    // if (inputID == "anotherTitle") {
+    //   setAnotherTitle(value);
+    // }
+  };
+
   return (
-    <>
-      <InputWithLabel 
-        todoTitle = {todoTitle} 
-        handleTitleChange={handleTitleChange} 
-        handleAddTodo={handleAddTodo}
-        label="Task Title "
-        isFocused
-      >
-        TodoTitle:  
-      </InputWithLabel>      
-    </>  
-  )
-};
+    <form onSubmit={handleAddTodo}>
+      <InputWithLabel
+        value={todoTitle}
+        onChange={onChangeInput}
+        label={"Title"}
+        name="title"
+        placeholder="Please enter your title"
+        type="text"
+      />
+      <InputWithLabel
+        value={dueDate}
+        onChange={onChangeInput}
+        label={"Due date"}
+        name="dueDate"
+        placeholder="YYYY-MM-DD"
+        type="date"
+      />
+      {/* <InputWithLabel
+        value={anotherTitle}
+        onChange={onChangeInput}
+        label={"Title 2"}
+        name="anotherTitle"
+        placeholder="Please enter your title"
+        type="text"
+      /> */}
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
 export default AddToDoForm;
--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,45 +1,20 @@
-import React from 'react';
+import React from "react";
 // import addTasks from './AddTodoForm';
 
-function InputWithLabel({ todoTitle, handleTitleChange, handleAddTodo, children, label, isFocused }) {
-  
-  const inputRef = React.useRef();
-
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-
+function InputWithLabel({ value, onChange, label, name, placeholder, type }) {
   return (
     <>
-      <form onSubmit={handleAddTodo}>
-      
       {/* below will use label value first, but if empty, then will use children = TodoTitle   */}
-      <label htmlFor="todoTitle">{label || children}</label>
-      <input 
-        id={todoTitle} 
-        type="text" 
-        name="title" 
-        value={todoTitle}
-        placeholder = "title"
-        onChange={handleTitleChange}
-        ref={inputRef}        
+      <label>{label}</label>
+      <input
+        id={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
       />
-      
-      <label htmlFor="taskDueDate">Due Date</label>
-      <input id="taskDueDate" placeholder = "YYYY-MM-DD"/>
-      <button type="submit" onClick={handleAddTodo}>Add</button>   
-      <span> </span>
-     
-      </form>      
     </>
-  )
-
+  );
 }
 
-
 export default InputWithLabel;
-
-
-
