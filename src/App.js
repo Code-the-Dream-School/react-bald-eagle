26021980f5298c8064dcbba8bc64e4e23b67803e
koral14 <105457134+koral14@users.noreply.github.com>
2023-02-27 17:25:50 -0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TodoContainer from './components/TodoContainer';
 import Home from './components/Home';
 import Header from './components/Header';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [toggleAscDescSorting, setToggleAscDescSorting] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/ltg' exact element={<TodoContainer tableName="Long Term Goals" />} />
-        <Route path='/stg' exact element={<TodoContainer tableName="Short Term Goals" />} />
-        <Route path='/new' exact element={<h1>New Todo List</h1>} />
+        <Route path='/' exact element={
+          <Home 
+            todoList={todoList} 
+            setTodoList={setTodoList} 
+            toggleAscDescSorting={toggleAscDescSorting} 
+            setToggleAscDescSorting={setToggleAscDescSorting} 
+            isLoading={isLoading} 
+            setIsLoading={setIsLoading} 
+          />
+        } />
+        <Route path='/ltg' exact element={
+          <TodoContainer 
+            tableName="Long Term Goals" 
+            todoList={todoList} 
+            setTodoList={setTodoList} 
+            toggleAscDescSorting={toggleAscDescSorting} 
+            setToggleAscDescSorting={setToggleAscDescSorting} 
+            isLoading={isLoading} 
+            setIsLoading={setIsLoading} 
+          />
+        } />
+        <Route path='/stg' exact element={
+          <TodoContainer 
+            tableName="Short Term Goals" 
+            todoList={todoList} 
+            setTodoList={setTodoList} 
+            toggleAscDescSorting={toggleAscDescSorting} 
+            setToggleAscDescSorting={setToggleAscDescSorting} 
+            isLoading={isLoading} 
+            setIsLoading={setIsLoading} 
+          />
+        } />
       </Routes>
   </BrowserRouter>
   );
