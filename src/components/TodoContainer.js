a4a92d378fa30254995be7d2120f703f94ed692e
koral14 <105457134+koral14@users.noreply.github.com>
2023-02-27 17:12:50 -0800

    updated TodoContainer.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useEffect }  from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 import './TodoContainer.css';
+
 import PropTypes from 'prop-types';
 
-const TodoContainer = ({ tableName }) => {
+const TodoContainer = ({ tableName, todoList, setTodoList, toggleAscDescSorting, setToggleAscDescSorting, isLoading, setIsLoading }) => {
     const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${tableName}`
-
-    const [todoList, setTodoList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
     const fetchTableData = async () => {
         const response = await fetch(`${url}`, {
@@ -20,11 +18,17 @@ const TodoContainer = ({ tableName }) => {
         const data = await response.json();
         setTodoList(data.records);
         setIsLoading(false);
+
+        data.records.sort((objectA, objectB) => {
+            if (objectA.fields.Title < objectB.fields.Title) return -1;
+            else if (objectA.fields.Title > objectB.fields.Title) return 1;
+            else return 0;  
+        })
     }
 
     useEffect(() => {
         fetchTableData();   
-    }, [tableName]);
+    }, [tableName, isLoading]); 
 
     useEffect(() => {
         if(!isLoading) {
@@ -32,30 +36,6 @@ const TodoContainer = ({ tableName }) => {
         }
     }, [todoList, isLoading]);
 
-    const addTableData = async (newFields) => {
-        const res = await fetch(
-            `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${tableName}`,
-            {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    records: [
-                        {
-                            fields: {
-                                ...newFields,
-                            },
-                        },
-                    ],
-                }),
-            }
-        );
-        const data = await res.json();
-        return data;
-    };
-        
     const deleteTableData = async (id) => {
         const res = await fetch(
         `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${tableName}/${id}`,
@@ -79,7 +59,6 @@ const TodoContainer = ({ tableName }) => {
     };
         
     const updateTodo = async (editedRow) => {
-        console.log("editedRow is: ", editedRow)
         const res = await fetch(
             `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${tableName}`,
             {
@@ -96,32 +75,43 @@ const TodoContainer = ({ tableName }) => {
             }
         );
         const data = await res.json();
+
+        data.records.sort((objectA, objectB) => {
+            if (objectA.fields.Title < objectB.fields.Title) return -1;
+            else if (objectA.fields.Title > objectB.fields.Title) return 1;
+            else return 0;  
+        })
+        
         return data;
       };
-    
+
     const ContainersSubComponent = () => {
+        
         return (
-           <>
-                <h1 className='item1'>{tableName}</h1>
+            <>
+                <h1 className='h1__todoContainer'>{tableName}</h1>
                 <div className='add__form__container'>
-                    <AddTodoForm onAddTodo={addTableData} todoList={todoList} setTodoList={setTodoList}/> 
+                    <AddTodoForm 
+                        tableName={tableName}
+                        todoList={todoList} 
+                        setTodoList={setTodoList} 
+                        toggleAscDescSorting={toggleAscDescSorting}
+                        setToggleAscDescSorting={setToggleAscDescSorting}
+                    /> 
                 </div>
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
-                    <div className='todoList__wrapper'>
-                        <TodoList 
-                            todoList={todoList} 
-                            onRemoveTodo={removeTodo} 
-                            onUpdateTodo={updateTodo}
-                            setTodoList={setTodoList}
-                        />
-                    </div>  
+                    <TodoList 
+                        todoList={todoList} 
+                        setTodoList={setTodoList}
+                        onRemoveTodo={removeTodo} 
+                        onUpdateTodo={updateTodo}
+                    />
                 )}
             </>
         )
     }
-
     return (
         <>
             <ContainersSubComponent tableName={tableName} />
@@ -131,6 +121,12 @@ const TodoContainer = ({ tableName }) => {
 
 TodoContainer.propTypes = {
     tableName: PropTypes.string,
+    todoList: PropTypes.array,
+    setTodoList: PropTypes.func,
+    toggleAscDescSorting: PropTypes.bool,
+    setToggleAscDescSorting: PropTypes.func,
+    isLoading: PropTypes.bool,
+    setIsLoading: PropTypes.func,
 }
 
 export default TodoContainer;
