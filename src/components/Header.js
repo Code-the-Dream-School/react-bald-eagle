ab4d8db77754d70927c58699c775a47e5e4d0d58
koral14 <105457134+koral14@users.noreply.github.com>
2023-02-27 17:21:11 -0800

    updated Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
     return (
         <>
             <nav className={style.navBar}>
-                <ul>
+                <img src="https://cdn.pixabay.com/photo/2020/01/21/18/39/todo-4783676_960_720.png" alt='list'/>
+                <ul className={style.ulLinks}>
                     <Link to="/stg" className={style.link}>Short Term Goals</Link>
                     <Link to="/" className={style.link}>Home</Link>
                     <Link to="/ltg" className={style.link}>Long Term Goals</Link>
                 </ul>
             </nav>
-            <img src="https://cdn.pixabay.com/photo/2020/01/21/18/39/todo-4783676_960_720.png" alt='list'/>
         </>
     )
 }
